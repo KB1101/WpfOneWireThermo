759e1913e01d5b675efdcb9fec3fdd2964d5b2b3
Kornel <Kornel@KB>
2017-05-18 03:16:54 +0200

    Visual Config

--- WpfOneWireThermo/MainWindow.xaml.cs
+++ WpfOneWireThermo/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace WpfOneWireThermo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Boolean isToolWindowHide;
+        
         public MainWindow()
         {
+            this.AllowsTransparency = true;
             InitializeComponent();
+            this.isToolWindowHide = true;
+        }
+        public MainWindow(Boolean allow)
+        {
+            this.AllowsTransparency = allow;
+            InitializeComponent();
+        }
+
+        //Window always on top
+        private void WindowDeactivated_Event(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+        //if keyboard click
+        private void DS18B20SensorViewer_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    this.Close();
+                    break;
+                case Key.OemPlus:
+                    this.VisualOpacity += (this.VisualOpacity < 1.0)? 0.1 : 0.0;
+                    break;
+                case Key.OemMinus:
+                    this.VisualOpacity -= (this.VisualOpacity > 0.0) ? 0.1 : 0.0;
+                    break;
+
+            }
+
+
+        }
+        //double click on wondow then show toolwindow (???)
+        private void DS18B20SensorViewer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (this.isToolWindowHide) {
+
+                this.isToolWindowHide = false;
+            }
+            else
+            {
+
+                this.isToolWindowHide = true;
+            }
+        }
+
+        private void DS18B20SensorViewer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
