d810bcca041458fcc18c1f546b6cbda35da12c13
Kornel <Kornel@KB>
2017-05-19 16:51:48 +0200

    DS18B20 class added; Some other changes;

---
+++ WpfOneWireThermo/Classes/DS18B20.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; 
+
+namespace WpfOneWireThermo
+{
+    /* Only one 1-Wire sensor on bus !!!!!! */
+    class DS18B20
+    {
+        private SerialPortOneWire oneWire;
+        public String[] oneWireAdapterPort { get; }
+        public DS18B20()
+        {
+            this.oneWire = new SerialPortOneWire();
+            this.oneWireAdapterPort = oneWire.AviavailablePorts();
+            this.oneWire = null;
+        }
+        public void OneWireRun(String port)
+        {
+            this.oneWire = new SerialPortOneWire(port);
+            try
+            {
+                this.oneWire.OneWireInit();
+            } catch (Exception) { }
+        }
+        public byte[] GetSensorAddress()
+        {
+            byte[] address = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            try
+            {
+                if (oneWire.Reset() != 0xFF)
+                {
+                    oneWire.WriteByte(0x33);
+                    if (oneWire.ReadByte() != 0x33) return address;
+
+                    for (var i = 0; i < 8; i++)
+                    {
+                        oneWire.WriteByte(0xFF);
+                        address[7 - i] = oneWire.ReadByte();
+                    }
+
+                }  
+            } catch (Exception){
+                throw new NotImplementedException();
+            }
+
+            return address;
+        }
+        public double GetSensorTemperature()
+        {
+            double result = -127F;
+            try
+            {
+                byte resetState = oneWire.Reset();
+                // if device is present
+                if (resetState >= 0x10 && resetState <= 0x90)
+                {
+                    oneWire.WriteByte(0xCC); // Skip ROM command
+                    if (oneWire.ReadByte() != 0xCC) return result;
+                    oneWire.WriteByte(0x44); // Convert temperature command
+                    if (oneWire.ReadByte() != 0x44) return result;
+
+                    Thread.Sleep(800); // Wait for DS18B20 
+
+                    // Get the Data
+                    oneWire.Reset();
+                    oneWire.WriteByte(0xCC); // Skip ROM command
+                    if (oneWire.ReadByte() != 0xCC) return result;
+                    oneWire.WriteByte(0xBE); // Tell sensor to take a reading
+                    if (oneWire.ReadByte() != 0xBE) return result;
+
+                    byte LSB = 0xFF, MSB = 0xFF;
+                    //start reading 
+                    oneWire.WriteByte(0xFF);
+                    LSB = oneWire.ReadByte();
+                    oneWire.WriteByte(0xFF);
+                    MSB = oneWire.ReadByte();
+
+                    int rawTemperature = (UInt16)((UInt16)LSB | (UInt16)(MSB << 8));
+
+                    // temperature conversion from int32 into double
+                    result = 1F;
+                    if ((rawTemperature & 0x8000) > 0)
+                    {
+                        rawTemperature = (rawTemperature ^ 0xffff) + 1;
+                        result = -1F;
+                    }
+                    result *= (double)(6F * rawTemperature + rawTemperature / 4F) / 100F;
+                }
+ 
+            } catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
+            return result;
+        }
+    }
+}
---
+++ WpfOneWireThermo/Classes/SerialPortOneWire.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace WpfOneWireThermo
+{
+    class SerialPortOneWire
+    {
+        private SerialPort serialPort; // serial port handle
+
+        // serial port configuration for OneWire communication via RS232 
+        private String portName = "COM1";
+        private const Int32 baudRate = 115200;
+        private const Parity parity = Parity.None;
+        private const Int32 dataBits = 8;
+        private const StopBits stopBits = StopBits.One;
+
+        private String[] portNames; 
+
+        public SerialPortOneWire()
+        {
+            serialPort = new SerialPort();
+            serialPort.ReadTimeout = serialPort.WriteTimeout = 1000;
+        }
+        public SerialPortOneWire(String portName)
+        {
+            serialPort = new SerialPort(portName,baudRate,parity,dataBits,stopBits);
+            serialPort.ReadTimeout = serialPort.WriteTimeout = 1000;
+        }
+        ~SerialPortOneWire()
+        {
+            if (this.serialPort.IsOpen) this.serialPort.Close();
+            this.serialPort.Dispose();
+            this.serialPort = null;
+        }
+        /*
+         * The port names are obtained from the system registry. 
+         * If the registry contains stale or otherwise incorrect data then AviavailablePorts method will return incorrect data.
+         * The order of port names returned from GetPortNames is not specified.
+         * This method returns String[]
+         */
+        public String[] AviavailablePorts()
+        {
+            portNames = SerialPort.GetPortNames();
+            return portNames;
+        }
+        public Boolean OneWireInit()
+        {
+            try
+            {
+                this.serialPort.DtrEnable = true;
+                this.serialPort.Open();
+            } catch(Exception exception)
+            {
+                Console.Error.WriteLine(exception);
+                throw exception;
+                
+            }
+            return serialPort.IsOpen;
+        }
+        public Boolean OneWireDeinit()
+        {
+            try
+            {
+                serialPort.Close();
+            } catch (Exception exception)
+            {
+                Console.Error.WriteLine(exception);
+                throw exception;
+            
+            }
+            return serialPort.IsOpen;
+        }
+        public void SetPortName(String portName)
+        {
+            try
+            {
+                Boolean portState = serialPort.IsOpen;
+                if (portState) serialPort.Close();
+                serialPort.PortName = portName;
+                this.portName = portName;
+                if (portState) serialPort.Open();
+            } catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void WriteByte(byte bajt)
+        {
+            byte[] temp = new byte[8];
+            for(var i = 0; i < 8; i++)
+            {
+                temp[i] = ( (int)(bajt & 0x01) == 1) ? (byte)0xFF : (byte)0x00;
+                bajt = (byte)((int)bajt >> 1);
+            }
+            try
+            {
+                if (!serialPort.IsOpen) serialPort.Open();
+                serialPort.DtrEnable = true;
+                serialPort.Write(temp, 0, 8);
+            } catch(TimeoutException tex)
+            {
+                Console.Error.WriteLineAsync(tex.ToString());
+                throw tex;
+            } 
+         }
+        public byte ReadByte()
+        {
+            byte[] temp = new byte[8];
+            int bajt = 0;
+            try
+            {
+                if (!serialPort.IsOpen) serialPort.Open();
+                serialPort.DtrEnable = true;
+                serialPort.Read(temp, 0, 8);
+
+                for (var i = 0; i < 8; i++)
+                {
+                    bajt = bajt << 1;
+                    bajt += (temp[7 - i] == (byte)0xFF) ? 1 : 0;
+                }
+            } catch (TimeoutException tex)
+            {
+                Console.Error.WriteLineAsync(tex.ToString());
+                throw tex;
+            }
+            return (byte)bajt;
+        }
+        public byte Reset(Boolean rfb = false)
+        {
+            byte returnedByte = 0xFF;
+            byte[] bajt = new byte[1];
+            bajt[0] = 0xF0;
+
+            try
+            {
+                serialPort.BaudRate = 9600;
+                if (!serialPort.IsOpen) serialPort.Open();
+                serialPort.DtrEnable = true;
+
+                serialPort.Write(bajt, 0, 1);
+                if(rfb) returnedByte = this.ReadByte();
+                else returnedByte = (byte)serialPort.ReadByte();
+
+                serialPort.BaudRate = 115200;
+
+            } catch (TimeoutException tex)
+            {
+                Console.Error.WriteLineAsync(tex.ToString());
+                throw tex;
+            }
+            return returnedByte;
+        }
+    }
+}
